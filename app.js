445ed296f1943c9c36094a444cfc6c0e6b9f93fe
SHAHZAIN <zgp3956@gmail.com>
2024-10-03 21:38:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// Assiment # 1
+// javascript ALERT
+//  alert("Erorr! please enter a valid password.");
+
+// public class Main {
+//     public static void main(String[] args) {
+//       String name = "Jhone Doe";
+//       System.out.println(name);
+//     }
+//   }
+
+//    ASSIMENT # 2
+
+//    VARIABLES FOR STRINGS
+
+//       var name1 = "Jhon Doe";
+//       console.log(name1);
+
+//  var greeting = "15 years old";
+// console.log (greeting)
+
+
+// ASSIMENT # 3
+
+// USER INPUT & CONDUTIONAL STATEMENT 
+
+//  Create an integer variable that will store the number we get from the user
+// int myNum;
+
+//  Ask the user to type a number
+// printf("Type a number: \n");
+
+// Get and save the number the user types
+// scanf("%d", &myNum);
+
+//  Output the number the user typed
+// printf("Your number is: %d", myNum);
+
+
+// ASSIMENT NO # 23 
+
+// STRING FINDING SEGMENT
+
+//  var text = "hello, i am salyani mass it traning student malir campus.";
+//  console.log(text.replaceALL ("saylani","smit"));
+
+//  var text = "hello, i am salyani mass it traningi student.";
+//  var text = prompt("Enter text");
+//  for (var i=0; i<=2; i++) {
+//     if (text=="saylani"){
+//         alert("found");
+//     } 
+//  }
+
+
+
